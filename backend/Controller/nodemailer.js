38d734a7e3d5268330c5a3e1ab4e65be274aa5a9
Samit Raj <sunnybhasneiya@gmail.com>
2026-07-16 15:22:31 +0530

    feat: replace gmail with mailtrap configs

--- backend/Controller/nodemailer.js
+++ backend/Controller/nodemailer.js
@@ -2,22 +2,17 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
       const transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 587,
-            secure: false,
-            auth : {
-                  user : process.env.EMAIL_USER,
-                  pass : process.env.EMAIL_PASS,
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525, 
+            auth: {
+                  user: process.env.MAILTRAP_USER, 
+                  pass: process.env.MAILTRAP_PASS  
             }
-            // ,
-            // tls: {
-            //       rejectUnauthorized: false
-            // }
       });
 
       const mailOptions = {
-            from : `"EasyGo Cars" <${process.env.EMAIL_USER}>`,
-            to : options.email,
+            from: `"EasyGo Rentals" <test@easygorentals.com>`, 
+            to: options.email, 
             subject: options.subject,
             text: options.message,
             html: options.html,
